fc082a9c02fbe6f5494f6c39070af05236c573b5
canaan901 <60493091+canaan901@users.noreply.github.com>
2020-01-30 21:27:50 -0600

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h2>This notebook are mainly used for capstone project</h2>
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
